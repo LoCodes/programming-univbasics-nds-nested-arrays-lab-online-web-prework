745ad6774c651c00825b80534ec442e917fe4b54
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-22 09:44:44 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -19,7 +19,7 @@ CONVENTIONAL_PRODUCE = [
 def assembled_matrix
   assembled_matrix = [["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], [ "Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]]
   
-  assembled_matrix[CONVENTIONAL_PRODUCE][ORGANIC_PRODUCE]
+  [CONVENTIONAL_PRODUCE][ORGANIC_PRODUCE]
   # Build an array that contains both of the above arrays
   # This matrix will represent a produce storage room
   # Organic standards require that organic products be stored ABOVE conventional, not the other way around
